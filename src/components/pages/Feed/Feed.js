467d9571bef7f5ced559659fe88ca4a989844c8d
Sai Teja Jammalamadaka <tsjamm@gmail.com>
2017-10-06 16:02:13 +0530

    Rendering with NativeBase UI for the Container and Header

--- src/components/pages/Feed/Feed.js
+++ src/components/pages/Feed/Feed.js
@@ -1,8 +1,9 @@
 
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import { Container, Header, Content, Title, Body, Left, Right, Button, Icon, Text } from 'native-base';
 
-import { Header } from '../../common';
+//import { Header } from '../../common';
 import FeedItemList from  './FeedItemList';
 
 class Feed extends Component {
@@ -15,15 +16,24 @@ class Feed extends Component {
         var { selectedSource } = this.props;
 
         return (
-            <View style={styles.containerViewStyle} >
-                <Header 
-                    backButtonText="<" 
-                    onBackButtonTap={this.resetSource.bind(this)}
-                >
-                    {selectedSource.title}
+            <Container>
+                <Header>
+                    <Left>
+                        <Button transparent
+                            onPress={() => this.resetSource()}
+                        >
+                            <Icon name='arrow-back' />
+                        </Button>
+                    </Left>
+                    <Body>
+                        <Title>{selectedSource.title}</Title>
+                    </Body>
+                    <Right />
                 </Header>
-                <FeedItemList sourceURL={selectedSource.url} />
-            </View>
+                <Content>
+                    <FeedItemList sourceURL={selectedSource.url} />
+                </Content>
+            </Container>
         )
     }
 }
